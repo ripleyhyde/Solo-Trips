0a11d40d075590d0627f9722dc86505f83e130d4
ripleyhyde <you@example.com>
2022-02-22 10:53:12 +0300

    need to make TripCreate component

--- soloTrips/App.js
+++ soloTrips/App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import TripList from "./components/TripList";
+import { ImageBackground } from "react-native";
+import TripCreate from "./components/TripCreate";
 
 export default function App() {
   return (
+    // <ImageBackground
+    //   source={{
+    //     uri: "https://cdn1.epicgames.com/epic/offer/Journey_SmallSize-2580x1450-75345be2b7101291982f1dcfcedaadbd.jpg",
+    //   }}
+    //   style={{ height: 50, width: 50 }}
+    // >
     <View style={styles.container}>
-      <TripList />
+      {/* <TripList /> */}
+      <TripCreate />
     </View>
+    // </ImageBackground>
   );
 }
 
---
+++ soloTrips/components/TripCreate.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const TripCreate = () => {
+  return (
+    <View>
+      <Text>TripCreate</Text>
+    </View>
+  );
+};
+
+export default TripCreate;
+
+const styles = StyleSheet.create({});
